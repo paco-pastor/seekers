553d22fd864af5aa697fca3546c5d56fe1324277
Paco-Pastor <paco.pastor@hippolyte-rh.fr>
2024-11-05 16:34:33 +0100

    ➕ WIP use bcrypt & jwt for login

--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -2,30 +2,26 @@ import { account, Account } from "../../db/schema/account";
 import database from "../../db/database";
 import { eq } from "drizzle-orm";
 import { json } from '@sveltejs/kit'
-import crypto from 'crypto';
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 export async function POST({ request } : { request: Request }) {
   const { email, password } = await request.json();
-  try {
     const result: Account[] = await database.select().from(account).where(eq(account.email, email));
     if (result.length === 0) {
       return json({ error: "Account not found" });
     }
     const account = result[0];
-    const hash = crypto.createHash('sha256');
-    hash.update
-    const hashedPassword = hash.digest('hex');
-    if (account.password !== hashedPassword) {
+
+    if (await bcrypt.compare(password, account.password) === false) {
       return json({ error: "Invalid password" });
     }
 
+    const token = jwt.sign({ email }, 'SECRET_HERE', { expiresIn: '1h' });
+
     return json({
       message: `Successfully logged in with email : ${email}`,
+      token
     });
-  }
-  catch (e) {
-    return json({ error: e.message });
-  }
-
 
 }
