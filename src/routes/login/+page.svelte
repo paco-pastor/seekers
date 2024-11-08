ce5ef09f18a2d1e97fc768da6e2955b02d33d285
Paco-Pastor <paco.pastor@hippolyte-rh.fr>
2024-11-08 11:08:45 +0100

    🔒️ Login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,39 +1,43 @@
 <script>
-    import Button from "../../components/Button.svelte";
-    import Input from "../../components/Input.svelte";
-    let email = "";
-    let password = "";
+  import Button from "../../components/Button.svelte";
+  import Input from "../../components/Input.svelte";
+  let email = "";
+  let password = "";
 
-    // WIP
-    function submit(event) {
-       fetch("/login", {
-           method: "POST",
-           headers: {
-               "Content-Type": "application/json"
-           },
-           body: JSON.stringify({email, password}) // Don't
-       }).then(response => {
-           if (response.ok) {
-               window.location.href = "/dashboard";
-           } else {
-               alert("Invalid email or password");
-           }
-       });
-    }
+  function submit(event) {
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }).then((response) => {
+      if (response.ok) {
+        event.locals.token = response.body.get("token");
+        window.location.href = "/dashboard";
+      } else {
+        alert("Invalid email or password");
+      }
+    });
+  }
 </script>
 
 <form>
-    <Input type="email" placeholder="Email" required bind:value={email} />
-    <Input type="password" placeholder="Password" required bind:value={password}/>
-    <Button type="submit" text="Click me" onClick={submit}/>
+  <Input type="email" placeholder="Email" required bind:value={email} />
+  <Input
+    type="password"
+    placeholder="Password"
+    required
+    bind:value={password}
+  />
+  <Button type="submit" text="Click me" onClick={submit} />
 </form>
 
 <style>
-    form {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 10px;
-    }
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+  }
 </style>
-
